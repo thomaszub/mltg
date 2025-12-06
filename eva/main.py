b3649d9f24e00ecb8261302d9bb1da02b6eed378
Thomas Zub <thomas.zub@outlook.de>
2025-12-06 17:18:10 +0100

    chore: downgrade python and add jax, flax, and optax

--- eva/main.py
+++ eva/main.py
@@ -1,5 +1,23 @@
+from flax import nnx
+import jax
+import jax.numpy as jnp
+
+
+class Linear(nnx.Module):
+    def __init__(self, din: int, dout: int, *, rngs: nnx.Rngs):
+        self.w = nnx.Param(rngs.params.uniform((din, dout)))
+        self.b = nnx.Param(jnp.zeros((dout,)))
+        self.din, self.dout = din, dout
+
+    def __call__(self, x: jax.Array):
+        return x @ self.w + self.b[None]
+
+
 def main():
-    print("Hello from eva!")
+    model = Linear(2, 5, rngs=nnx.Rngs(params=0))
+    y = model(x=jnp.ones((1, 2)))
+    print(y)
+    nnx.display(model)
 
 
 if __name__ == "__main__":
